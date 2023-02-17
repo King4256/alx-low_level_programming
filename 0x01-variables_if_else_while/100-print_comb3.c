00200f9babcf20afcf8e08673f7e8b54272754c9
King4256 <isacowusu@gmail.com>
2023-02-17 10:41:02 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 /**
- * main - Print two digit numbers in ascending order with commas and spaces
- *
- * Return: Always 0 (Success)
- */
+* main - Print two digit numbers in ascending order with commas and spaces
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int i, j;
-  for (i = 0; i <= 9; i++)
-{
-for (j = 1; j <= 9; j++
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 1; j <= 9; j++)
+        {
+            if (j > i)
+            {
+                putchar(i + '0');
+                putchar(j + '0');
+                if (i != 8)
                 {
-if (j > i)
-{
-putchar(i + '0');
-putchar(j + '0');
-if (i != 8)
-{putchar(',');
+                    putchar(',');
                     putchar(' ');
                 }
             }
         }
     }
     putchar('\n');
-
-    return (0);
+	return (0);
 }
