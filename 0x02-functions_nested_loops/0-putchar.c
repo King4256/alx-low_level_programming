c2140118612062610afe60be66d24934b4778ed7
King4256 <isacowusu@gmail.com>
2023-02-21 10:40:33 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
- * main - prints result
- * Return: 0 When successful
+ * main - main function
+ * _putchar: print the alphabets putchar
+ * Return: always 0
  */
 
 int main(void)
 {
-	int i;
-	char putty[] = "_putchar";
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(nuber[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
